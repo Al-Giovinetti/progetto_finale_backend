23e8f1061a76bd797a1ee913f344146920c4015f
DanielMarella <marella.daniel96@gmail.com>
2023-09-14 17:53:47 +0200

    fill the table user

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(Faker $faker): void
+    {
+
+        $gino = new User();
+        $gino->name = 'gino';
+        $gino->email = 'g@gmail.com';
+        $gino->password = Hash::make('12345');
+        $gino->save();
+
+
+        for($i = 0; $i < 100; $i++){
+            $newUser = new User();
+            $newUser->name = $faker->firstName();
+            $newUser->email = $faker->email();
+            $newUser->password = Hash::make($faker->password());
+            $newUser->save();
+        }
+    }
+}
