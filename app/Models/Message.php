eb397f3f374a7047d556831d88a743d0f86beb6f
Al-Giovinetti <giovinettialessio@gmail.com>
2023-09-15 11:17:49 +0200

    Add relation between messages and musicians

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -4,8 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Musician;
 
 class Message extends Model
 {
+    public function musician(){
+        return $this->belongsTo(Musician::class);
+    }
+
     use HasFactory;
 }
--- app/Models/Musician.php
+++ app/Models/Musician.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Message;
 
 class Musician extends Model
 {
@@ -12,6 +13,11 @@ class Musician extends Model
     public function user(){
         return $this->belongsTo(User::class);
     }
+
+    public function messages(){
+        return $this->hasMany(Message::class);
+    }
+
 
 }
 
