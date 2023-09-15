81b2bfcd4a9eb31d84e112b034e38456e818e2ad
Al-Giovinetti <giovinettialessio@gmail.com>
2023-09-15 11:38:26 +0200

    Create Seeder for Messages table

---
+++ database/seeders/MessageSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Models\Message;
+use App\Models\Musician;
+
+class MessageSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(Faker $faker): void
+    {
+        $musicians= Musician::all();
+        foreach($musicians as $musician){
+            $newMessage = new Message();
+            $newMessage->musician_id = $musician->user_id;
+            $newMessage->name=$faker->name();
+            $newMessage->mail=$faker->email();
+            $newMessage->message= $faker->text();
+            $newMessage->save();
+        }
+    }
+}
