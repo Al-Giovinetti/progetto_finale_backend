f6f54f6a7564e3f2d2aa0e7e5aa533f3e17bd22b
Al-Giovinetti <giovinettialessio@gmail.com>
2023-09-17 16:17:48 +0200

    Modified seeder of the musician sponsor table

--- database/seeders/MusicianSponsorSeeder.php
+++ database/seeders/MusicianSponsorSeeder.php
@@ -9,24 +9,35 @@ use App\Models\Sponsor;
 use App\Models\Musician;
 use App\Models\MusicianSponsor;
 
-
-
 class MusicianSponsorSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
+    
     public function run(Faker $faker): void
     {
-        $sponsors = Sponsor::all();
-        $musicians = Musician::all()->pluck('user_id');
+        $sponsors = Sponsor::all()->pluck('id');
+        $musicians = Musician::all();
 
-        foreach ($sponsors as $sponsor) {
-            $sponsor->musicians()->sync([$faker->randomElement($musicians)]);
+        foreach ($musicians as $musician) {
+            $newMusicianSponsor = new MusicianSponsor();
+            $newMusicianSponsor->musician_id = $musician->user_id;
+            $newMusicianSponsor->sponsor_id = $faker->randomElement($sponsors);
+            $newMusicianSponsor->sponsor_start = $faker->dateTimeBetween('-30 week');
 
+            $dataStart= $newMusicianSponsor->sponsor_start->format('Y-m-d H:i:s');
+            var_dump($dataStart);
+            if($newMusicianSponsor->sponsor_id == 1){
+                $newMusicianSponsor->sponsor_end = date('Y-m-d H:i:s',strtotime('+1 day',strtotime($dataStart)));
+            }elseif($newMusicianSponsor->sponsor_id == 2){
+                $newMusicianSponsor->sponsor_end =date('Y-m-d H:i:s',strtotime('+3 day',strtotime($dataStart)));
+            }else{
+                $newMusicianSponsor->sponsor_end =date('Y-m-d H:i:s',strtotime('+6 day',strtotime($dataStart)));
+            }
+
+            $newMusicianSponsor->save();
         }
-
-
-
     }
+    
 }
