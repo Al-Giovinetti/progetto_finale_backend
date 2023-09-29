c02f67f121ccdf36db238da00f724b3e43f38ed8
DanielMarella <marella.daniel96@gmail.com>
2023-09-29 17:36:38 +0200

    added fillable

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -7,6 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
+
+    protected $fillable = [
+        'musician_id',
+        'vote',
+        'content',
+
+    ];
     public function musician(){
         return $this->belongsTo(Musician::class);
     }
